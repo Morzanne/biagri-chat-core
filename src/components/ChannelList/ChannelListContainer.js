bd9cea235c0b810b8068d8ece2369c58840deff4
Aurélien <a.grenet@biagri.com>
2020-03-18 15:16:15 +0100

    dispatch the open chat event to subscribers

--- src/components/ChannelList/ChannelListContainer.js
+++ src/components/ChannelList/ChannelListContainer.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import { withChatContext } from 'stream-chat-react';
 import { ChannelList } from './ChannelList';
+import { EventHandler } from '../../lib/eventHandler';
 
 class ChannelListContainer extends Component {
-  state = {
-    isChatBoxClosed: false
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      isChatBoxClosed: false
+    };
+    EventHandler.dispatch('openChat', {
+      onClick: this.onClick
+    });
+  }
 
   onClick = () => {
     const { isChatBoxClosed } = this.state;
     const currentChatBoxState = isChatBoxClosed;
+
     this.setState({ isChatBoxClosed: !currentChatBoxState });
+    EventHandler.dispatch('openChat', {
+      currentChatBoxState: !currentChatBoxState,
+      onClick: this.onClick
+    });
   };
 
   render() {
